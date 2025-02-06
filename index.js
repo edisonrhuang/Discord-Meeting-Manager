e4fe60e208713c8698e59c47bcef0d7eb78c5276
Arashi <edisonrhuang@vt.edu>
2025-02-06 12:37:28 -0500

    detect-module

--- index.js
+++ index.js
@@ -7,12 +7,4 @@ client.on(Events.ClientReady, client => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
-});
-
 client.login(process.env.DEV_TOKEN);
