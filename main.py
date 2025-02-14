1c8c71aa31d4afa6ba53e6b65143abf14b5c43b4
Arashi <edisonrhuang@vt.edu>
2025-02-14 17:36:28 -0500

    Project intialization

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(f"{os.getenv('DEV_TOKEN')}")
