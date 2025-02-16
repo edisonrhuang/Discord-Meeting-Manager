c051d9c1690606226e0bd52b9ec02fa7b7bb1308
davidvillon04 <davidv04@vt.edu>
2025-02-15 21:26:26 -0500

    Update hello.py

--- cogs/hello.py
+++ cogs/hello.py
@@ -7,17 +7,21 @@ from discord.ext import commands
 # SERVER ID
 GUILD_ID = discord.Object(id=1337086900409864224)
 
+# Create a Cog for the hello command
 class Hello(commands.Cog):
-    """A Cog for the /hello command."""
-
+    # Initialize the Cog with the bot instance
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    # Define the hello command as a slash command
     @app_commands.command(name="hello", description="Say hello!")
     @app_commands.guilds(GUILD_ID) # Register command to the server
+    
+    # Define the command function
     async def hello(self, interaction: discord.Interaction):
         # Respond to the slash command with a mention.
         await interaction.response.send_message(f"Hello {interaction.user.mention}!")
 
+# Setup function to add the Cog to the bot
 async def setup(bot):
     await bot.add_cog(Hello(bot))
