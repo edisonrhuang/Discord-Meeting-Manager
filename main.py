540d14a1d0c53ad27971926d5aa753d50813822c
Arashi <edisonrhuang@vt.edu>
2025-02-17 14:11:12 -0500

    sqlite setup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, os, dotenv
+import discord, os, dotenv, aiosqlite
 from discord.ext import commands
 
 dotenv.load_dotenv()
@@ -18,14 +18,20 @@ class Client(commands.Bot):
                 except Exception as e:
                     print(f"Failed to load extension {extension}: {e}")
     
-    async def on_ready(self):        
+    async def on_ready(self):
+        self.db = await aiosqlite.connect("database.db")
+        cursor = await self.db.cursor()
+
+        await cursor``.execute("")
+        await self.db.commit()
+
         # Sync the command tree for the specific guild so that the slash commands are registered immediately.
         try:
             synced = await self.tree.sync(guild=GUILD_ID)
             print(f"Synced {len(synced)} command(s) for guild {GUILD_ID.id}")
         except Exception as e:
             print(f"Error syncing commands: (e)")
-        print(f'Logged on as {self.user}')  
+        print(f'Logged on as {self.user}')
 
 intents = discord.Intents.default()
 intents.message_content = True
